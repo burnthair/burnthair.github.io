ce15b60554a17949e7bd01f1a71a0fb1a7c04865
mv <edwardsmichaelv@gmail.com>
2019-03-20 21:36:13 -0400

    add second section, swap out svgs, clean up css

--- js/script.js
+++ js/script.js
@@ -5,16 +5,16 @@
 // sticky nav w/ intersection observer
 //
 //
-var hamburgerHand = document.querySelector(".hamburger-hand");
+var hamburgerHand = document.querySelector(".hamburger-menu");
 var hiddenLinks = document.querySelector(".hidden-links");
 var hamburgerClose = document.querySelector(".hamburger-close");
 hamburgerHand.addEventListener('click', function () {
-  hamburgerHand.classList.add('hamburger-hand-active');
+  hamburgerHand.classList.add('hamburger-menu-active');
   hiddenLinks.classList.add('show-hidden-links');
   hamburgerClose.classList.add('hamburger-close-active');
 })
 hamburgerClose.addEventListener('click', function () {
-  hamburgerHand.classList.remove('hamburger-hand-active');
+  hamburgerHand.classList.remove('hamburger-menu-active');
   hiddenLinks.classList.remove('show-hidden-links');
   hamburgerClose.classList.remove('hamburger-close-active');
 })
